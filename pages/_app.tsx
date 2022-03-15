641cc8cef6983cc0d775c3a7497b3e85399a5901
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-03-15 21:53:10 +0000

    theme and session provider and next auth and next theme packs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
-
-export default MyApp
