6adacf1e2eeeb18e1f0fc5b1f8c3404e6d41aa33
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-03-15 23:15:43 +0000

    update info and remove session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,13 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import { SessionProvider } from 'next-auth/react';
+//import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
-    </SessionProvider>
   );
 }
