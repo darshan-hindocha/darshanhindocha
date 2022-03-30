2554ea2cd72cb4fb0f4c41ef82f0f6fbd53fb4bc
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-03-30 13:57:33 +0100

    config and clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import '../styles/globals.css';
 
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
-//import { SessionProvider } from 'next-auth/react';
+import type {AppProps} from 'next/app';
+import {ThemeProvider} from 'next-themes';
 
-export default function App({ Component, pageProps }: AppProps) {
-
-  return (
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
