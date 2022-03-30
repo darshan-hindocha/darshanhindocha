4d66e6636fd57e282139c89edece024f58f34999
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-03-30 23:05:49 +0100

    text changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import type {NextPage} from 'next'
-import Head from 'next/head'
-import Button from '@mui/material/Button';
 import {useRouter} from "next/router";
 import Container from "../components/Container";
 
 const Home: NextPage = () => {
-    const router = useRouter();
-
 
     return (
         <Container
             title="Home – Darshan Hindocha"
-            description="Blog (on Math, Music and Mind), Art Gallery, and Book Notes"
+            description="Blog, Art Gallery, and Book Notes"
         >
             <div className="flex flex-col items-left justify-around w-full max-w-2xl mx-auto mb-16">
                 <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
@@ -24,14 +20,14 @@ const Home: NextPage = () => {
                 </p>
                 <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
                     The direction and topics of the blog are loose for now. My general hope is to increase writing
-                    proficiency, and deepen an understanding of concepts through writing about them.
+                    proficiency, and deepen an understanding of the concepts I write about.
                 </p>
                 <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    More importantly, writing provides a compelling reason to be more thoughtful.
+                    More importantly, writing is a strong motivator to be more thoughtful.
                 </p>
 
                 <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    There will soon be book notes and an art gallery.
+                    Book notes and art gallery -- soon to come
                 </p>
 
             </div>
