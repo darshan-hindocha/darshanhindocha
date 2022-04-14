1f34790c365c7f5eec91176dbb8472b8d16a31ae
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-04-14 19:39:00 +0100

    photo added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type {NextPage} from 'next'
-import {useRouter} from "next/router";
 import Container from "../components/Container";
+import Image from "next/image";
 
 const Home: NextPage = () => {
 
@@ -9,11 +9,32 @@ const Home: NextPage = () => {
             title="Home – Darshan Hindocha"
             description="Blog, Art Gallery, and Book Notes"
         >
-            <div className="flex flex-col items-left justify-around w-full max-w-2xl mx-auto mb-16">
-                <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-                    Darshan Hindocha
-                </h1>
 
+
+            <div className="flex flex-col items-left justify-around w-full max-w-2xl mx-auto mb-16">
+                <div className="flex flex-col-reverse sm:flex-row items-start">
+                    <div className="flex flex-col pr-8">
+                        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+                            Darshan Hindocha
+                        </h1>
+                        <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+                            Software Engineer at{' '}
+                            <span className="font-semibold">AMEX</span>
+                        </h2>
+                        <p className="text-gray-600 dark:text-gray-400 mb-16">
+                            Interested in Data Science and Software Engineering
+                        </p>
+                    </div>
+                    <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+                        <Image
+                            alt="Darshan Hindocha"
+                            width={180}
+                            height={180}
+                            src="/Avatar1.jpeg"
+                            className="rounded-full"
+                        />
+                    </div>
+                </div>
                 <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
                     Currently (as of 30/03/2022), the blog page and blogs are ready. I&apos;ve been looking forward to
                     having this space for a while. I tried medium, but it didn&apos;t hit the spot.
