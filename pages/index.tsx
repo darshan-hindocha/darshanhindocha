b21271f8bc4d918a93a859c88af2a7ced0f3f3e1
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-04-14 19:47:42 +0100

    added featured posts on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type {NextPage} from 'next'
 import Container from "../components/Container";
 import Image from "next/image";
+import BlogPost from "../components/BlogPostPreview";
+import BookNotePreview from "../components/BookNotePreview";
 
 const Home: NextPage = () => {
 
@@ -9,7 +11,6 @@ const Home: NextPage = () => {
             title="Home – Darshan Hindocha"
             description="Blog, Art Gallery, and Book Notes"
         >
-
 
             <div className="flex flex-col items-left justify-around w-full max-w-2xl mx-auto mb-16">
                 <div className="flex flex-col-reverse sm:flex-row items-start">
@@ -35,21 +36,41 @@ const Home: NextPage = () => {
                         />
                     </div>
                 </div>
-                <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    Currently (as of 30/03/2022), the blog page and blogs are ready. I&apos;ve been looking forward to
-                    having this space for a while. I tried medium, but it didn&apos;t hit the spot.
-                </p>
-                <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    The direction and topics of the blog are loose for now. My general hope is to increase writing
-                    proficiency, and deepen an understanding of the concepts I write about.
-                </p>
-                <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    More importantly, writing is a strong motivator to be more thoughtful.
-                </p>
 
-                <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
-                    Book notes and art gallery -- soon to come
-                </p>
+                <div>
+                    <h1 className="font-bold text-1xl md:text-2xl tracking-tight mb-4 text-black dark:text-white">
+                        Recent Activity
+                    </h1>
+                    <BlogPost
+                        title="A Recall Process for Audiobooks"
+                        summary="Creating memorable journeys through audiobooks"
+                        slug="recalling-information-from-books"
+                    />
+                    <BookNotePreview
+                        title="Test Book Notes"
+                        summary="Just checking to see if this thing works..."
+                        slug="test-book-note"
+                    />
+
+                </div>
+                <div>
+                    <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
+                        Currently (as of 30/03/2022), the blog page and blogs are ready. I&apos;ve been looking forward
+                        to
+                        having this space for a while. I tried medium, but it didn&apos;t hit the spot.
+                    </p>
+                    <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
+                        The direction and topics of the blog are loose for now. My general hope is to increase writing
+                        proficiency, and deepen an understanding of the concepts I write about.
+                    </p>
+                    <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
+                        More importantly, writing is a strong motivator to be more thoughtful.
+                    </p>
+
+                    <p className="ml-2 text-m text-gray-700 dark:text-gray-300 mt-10">
+                        Book notes and art gallery -- soon to come
+                    </p>
+                </div>
 
             </div>
 
