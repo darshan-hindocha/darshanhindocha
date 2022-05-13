5a149e5dcc85dd45ebc852c76fe908b38dad5f83
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-05-13 22:50:18 +0100

    wider navbar in desktop

--- components/Container.tsx
+++ components/Container.tsx
@@ -62,7 +62,8 @@ export default function Container(props) {
             </Head>
 
             <nav
-                className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+                className="flex items-center justify-between w-full relative max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100"
+            >
                 <div className="ml-[-0.60rem]">
                     <MobileMenu />
                     <NavItem href="/" text="Home"/>
