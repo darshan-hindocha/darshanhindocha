e396ea43bf4167d348ec45949f01255893756390
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-05-29 22:40:44 +0100

    remove light and dark theme

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import NextLink from 'next/link';
 import {useRouter} from "next/router";
 import {useState, useEffect} from 'react';
-import {useTheme} from 'next-themes';
 import cn from 'classnames';
 import MobileMenu from "./MobileMenu";
 
@@ -29,7 +28,6 @@ function NavItem({href, text}) {
 
 export default function Container(props) {
     const [mounted, setMounted] = useState(false);
-    const {resolvedTheme, setTheme} = useTheme();
     const router = useRouter();
 
     useEffect(() => setMounted(true), []);
@@ -37,78 +35,44 @@ export default function Container(props) {
     const {children, ...customMeta} = props;
     const meta = {
         title: 'Darshan Hindocha',
-        description: `Programmer, (ex?)-Mathematician, (wannabe)-Writer and (sometimes)-Painter.`,
+        description: `Software Engineer, (ex?)-Mathematician, (wannabe)-Writer and (sometimes)-Painter.`,
         type: 'website',
         image: 'https://darshanhindocha.vercel.app/static/images/banner.png',
         ...customMeta
     };
 
     return (
-        <div className="bg-gray-50 dark:bg-gray-900 mt-20">
+        <div className="bg-gray-50 dark:bg-gray-900">
             <Head>
                 <title>{meta.title}</title>
-                <meta name="robots" content="follow, index" />
+                <meta name="robots" content="follow, index"/>
                 <meta content={meta.description} name="description"/>
-                <meta property="og:url" content={`https://darshanhindocha.vercel.app${router.asPath}`} />
-                <link rel="canonical" href={`https://darshanhindocha.vercel.app${router.asPath}`} />
+                <meta property="og:url" content={`https://darshanhindocha.vercel.app${router.asPath}`}/>
+                <link rel="canonical" href={`https://darshanhindocha.vercel.app${router.asPath}`}/>
                 <meta property="og:type" content={meta.type}/>
                 <meta property="og:site_name" content="Darshan Hindocha"/>
                 <meta property="og:description" content={meta.description}/>
                 <meta property="og:title" content={meta.title}/>
-                <meta property="og:image" content={meta.image} />
+                <meta property="og:image" content={meta.image}/>
                 {meta.date && (
                     <meta property="article:published_time" content={meta.date}/>
                 )}
             </Head>
 
             <nav
-                className="flex items-center justify-between w-full relative max-w-5xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100"
+                className="flex justify-between w-full relative border-gray-200 bg-gray-100 dark:border-gray-700 pt-8 pb-8 sm:pb-8  text-gray-900 bg-gray-50  dark:bg-gray-700 bg-opacity-60 dark:text-gray-100"
             >
                 <div className="m-4">
-                    <MobileMenu />
+                    <MobileMenu/>
                     <NavItem href="/" text="Home"/>
                     <NavItem href="/blog" text="Blog"/>
                     <NavItem href="/book-notes" text="Book Notes"/>
                     <NavItem href="/art" text="Art"/>
                     <NavItem href="/about" text="About"/>
                 </div>
-                <button
-                    aria-label="Toggle Dark Mode"
-                    type="button"
-                    className="w-9 h-9 m-4 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
-                    onClick={() =>
-                        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-                    }
-                >
-                    {mounted && (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            className="w-5 h-5 text-gray-800 dark:text-gray-200"
-                        >
-                            {resolvedTheme === 'dark' ? (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                />
-                            ) : (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                />
-                            )}
-                        </svg>
-                    )}
-                </button>
             </nav>
             <main
-                className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900"
+                className="flex flex-col mt-20 justify-center px-8 bg-gray-50 dark:bg-gray-900"
             >
                 {children}
             </main>
