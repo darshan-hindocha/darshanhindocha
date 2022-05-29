cfac14bc6c41a79a3ca5100f2aa6bd1d11684cb0
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-05-29 22:40:44 +0100

    remove light and dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,103 +1,75 @@
-const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+const {spacing, fontFamily} = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        // Simple 2 column grid
-        '2': 'repeat(2, minmax(100px, 1fr))'
-      },
-      colors: {
-        'blue-opaque': 'rgb(13 42 148 / 18%)',
-        gray: {
-          0: '#e5c9a3',
-          50: '#e5c9a3',
-          100: '#ddb887',
-          200: '#dfa86b',
-          300: '#cd974f',
-          400: '#c38636',
-          500: '#a7732e',
-          600: '#8b6027',
-          700: '#6f4d1f',
-          800: '#533917',
-          900: '#37260f'
+    content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                // Simple 2 column grid
+                '2': 'repeat(2, minmax(100px, 1fr))'
+            },
+            colors: {
+                'blue-opaque': 'rgb(13 42 148 / 18%)',
+                gray: {
+                    0: '#BABBBD',
+                    50: '#C8D1D9',
+                    100: '#BABBBD',
+                    200: '#F78166',
+                    300: '#238636',
+                    400: '#175225',
+                    500: '#30363D',
+                    600: '#30363D',
+                    700: '#161B22',
+                    800: '#0D1116',
+                    900: '#020408'
+                }
+            },
+            fontFamily: {
+                sans: ['IBM Plex Sans', ...fontFamily.sans]
+            },
+            css: {
+                color: 'colors.gray.200',
+                a: {
+                    color: 'colors.blue.400',
+                    '&:hover': {
+                        color: 'colors.blue.600'
+                    },
+                    code: {color: 'colors.blue.400'}
+                },
+                blockquote: {
+                    borderLeftColor: 'colors.gray.700',
+                    color: 'colors.gray.300'
+                },
+                'h2,h3,h4': {
+                    color: 'colors.gray.100',
+                    'scroll-margin-top': spacing[32]
+                },
+                hr: {borderColor: 'colors.gray.700'},
+                ol: {
+                    li: {
+                        '&:before': {color: 'colors.gray.500'}
+                    }
+                },
+                ul: {
+                    li: {
+                        '&:before': {backgroundColor: 'colors.gray.500'}
+                    }
+                },
+                strong: {color: 'colors.gray.100'},
+                thead: {
+                    th: {
+                        color: 'colors.gray.100'
+                    },
+                    borderBottomColor: 'colors.gray.600'
+                },
+                tbody: {
+                    tr: {
+                        borderBottomColor: 'colors.gray.700'
+                    }
+                }
+            },
         }
-      },
-      fontFamily: {
-        sans: ['IBM Plex Sans', ...fontFamily.sans]
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.700'),
-            a: {
-              color: theme('colors.blue.500'),
-              '&:hover': {
-                color: theme('colors.blue.700')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            'h2,h3,h4': {
-              'scroll-margin-top': spacing[32]
-            },
-            thead: {
-              borderBottomColor: theme('colors.gray.200')
-            },
-            code: { color: theme('colors.pink.500') },
-            'blockquote p:first-of-type::before': false,
-            'blockquote p:last-of-type::after': false
-          }
-        },
-        dark: {
-          css: {
-            color: theme('colors.gray.200'),
-            a: {
-              color: theme('colors.blue.400'),
-              '&:hover': {
-                color: theme('colors.blue.600')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            blockquote: {
-              borderLeftColor: theme('colors.gray.700'),
-              color: theme('colors.gray.300')
-            },
-            'h2,h3,h4': {
-              color: theme('colors.gray.100'),
-              'scroll-margin-top': spacing[32]
-            },
-            hr: { borderColor: theme('colors.gray.700') },
-            ol: {
-              li: {
-                '&:before': { color: theme('colors.gray.500') }
-              }
-            },
-            ul: {
-              li: {
-                '&:before': { backgroundColor: theme('colors.gray.500') }
-              }
-            },
-            strong: { color: theme('colors.gray.100') },
-            thead: {
-              th: {
-                color: theme('colors.gray.100')
-              },
-              borderBottomColor: theme('colors.gray.600')
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
-            }
-          }
-        }
-      })
-    }
-  },
-  variants: {
-    typography: ['dark']
-  },
-  plugins: [require('@tailwindcss/typography')]
+    },
+    plugins: [require('@tailwindcss/typography')]
 };
