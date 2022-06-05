2d9b49544b90233f527f78cab0ca62525d24df42
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-06-05 11:21:48 +0100

    add color scheme as accessible variables

---
+++ lib/theme.ts
@@ -0,0 +1,16 @@
+export const colorGrey = {
+    50: '#f0f6fc',
+    100: '#C2C8CF',
+    200: '#9BA2AA',
+    300: '#7D7D7F',
+    400: '#30363d',
+    700: '#151a22',
+    800: '#0d1116',
+    900: '#01040a',
+}
+
+export const colorGreen = {
+    light: '#f78166',
+    mid: '#238636',
+    dark: '#242710',
+}
