0703a68b30ef97946435c110d4a00bd7aadd54d9
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-06-05 11:20:46 +0100

    add theme for material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,59 @@
 import '../styles/globals.css';
 
 import type {AppProps} from 'next/app';
-import {ThemeProvider} from 'next-themes';
+import {ThemeProvider as Theme1} from 'next-themes';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {colorGrey} from "../lib/theme";
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            light: '#f78166',
+            main: '#238636',
+            dark: '#242710',
+            contrastText: '#fff'
+        },
+        secondary: {
+            light: '#a1887f',
+            main: '#5d4037',
+            dark: '#3e2723',
+            contrastText: '#fff'
+        },
+        info: {
+            light: '#ffab91',
+            main: '#ff5722',
+            dark: '#bf360c',
+        },
+        grey: {
+            50: '#f0f6fc',
+            100: '#C2C8CF',
+            200: '#9BA2AA',
+            300: '#7D7D7F',
+            400: '#393E46',
+            500: '#30363d',
+            600: '#232931',
+            700: '#151a22',
+            800: '#0d1116',
+            900: '#01040a',
+        },
+        background: {
+            paper: colorGrey[900],
+            default: colorGrey[900]
+        }
+    },
+    shape: {
+        borderRadius: 12
+    }
+});
+
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <ThemeProvider attribute="class">
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <Theme1 attribute="class">
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </Theme1>
     );
 }
